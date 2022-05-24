e97bcd3082761e336b22d122a07e0dd565233be6
Adam Rutkowski <hq@mtod.org>
2022-05-24 12:47:52 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wb.MixProject do
   def project do
     [
       app: :wb,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
