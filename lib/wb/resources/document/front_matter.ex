6bd6211a22457994f95728873b6a15dcabf02a83
Adam Rutkowski <hq@mtod.org>
2022-05-24 09:33:39 +0200

    Fix parsing front matter with excessive separators

--- lib/wb/resources/document/front_matter.ex
+++ lib/wb/resources/document/front_matter.ex
@@ -1,6 +1,6 @@
 defmodule WB.Resources.Document.FrontMatter do
   def extract(data) do
-    with [meta, content] <- String.split(data, "---\n", trim: true),
+    with [meta, content] <- String.split(data, "---\n", trim: true, parts: 2),
          {:ok, yaml} <- YamlElixir.read_from_string(meta) do
       {yaml, content}
     else
